2ceb8b24e25e523156a25df62a90df1aa01d9b43
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-15 16:22:33 -0500

    Car makes added
    Each car make found in the database is added to the make selectbox without repeats

--- main.js
+++ main.js
@@ -1,20 +1,7 @@
-document.addEventListener("DOMContentLoaded", function() {
-  const ROOT_URL = "http://localhost:3000/";
-  const DRIVERS_URL = `${ROOT_URL}/drivers`;
-  const CARS_URL = `${ROOT_URL}/cars`;
-
-  let driversSection = document.getElementById("drivers-section");
-  let allDrivers = [];
-  let allCars = [];
-
-  // Constructor Function for Models
-  function Driver(id, name, email) {
-    this.id = id;
-    this.name = name;
-    this.email = email;
-  };
-
-  function Car(id, make, model, year, description, imageUrl, driver_id) {
+class Car
+{
+  constructor(id, make, model, year, description, imageUrl, driver_id)
+  {
     this.id = id;
     this.make = make;
     this.model = model;
@@ -22,31 +9,42 @@ document.addEventListener("DOMContentLoaded", function() {
     this.description = description;
     this.imageUrl = imageUrl;
     this.driver_id = driver_id;
-  };
+  }
+}
 
-  // Fetch Driver data from backend
-  fetch(ROOT_URL)
-  .then(response => response.json())
-  .then(drivers => {
-    //console.log(drivers);
-    drivers.forEach(driver => {
-      let newDriver = new Driver(driver.id, driver.name, driver.email);
-      allDrivers.push(newDriver);
-    });
-  }); 
+const ROOT_URL = "http://localhost:3000/";
+const DRIVERS_URL = `${ROOT_URL}/drivers`;
+const CARS_URL = `${ROOT_URL}/cars`;
 
-  // Fetch Car data from backend
-  fetch(CARS_URL)
-  .then(response => response.json())
-  .then(cars => {
-    //console.log(cars);
-    cars.forEach(car => {
-      let newCar = new Car(car.id, car.make, car.model, car.year, car.description, car.imageUrl, car.driver_id);
-      allCars.push(newCar);
-    });
-  });
+let driversSection = document.getElementById("drivers-section");
+let driverCard = document.querySelector("div.driver.card");
+let makeSelectbox = document.querySelector("#make");
+let modelSelectbox = document.querySelector("#model");
+let yearSelectbox = document.querySelector("#year"); 
 
-  console.log(allDrivers);
-  console.log(allCars);
+// Fetch Car data from backend
+fetch(CARS_URL)
+.then(response => response.json())
+.then(cars => {
+  //console.log(cars);
+  for (let i = 0; i < cars.length; i++)
+  {
+    let newCar = new Car(cars[i].id, cars[i].make, cars[i].model, cars[i].year, cars[i].description, cars[i].driver_id);
+    let makeOption = document.createElement("option");
+    let modelOption = document.createElement("option");
+    let yearOption = document.createElement("option");
 
+    // Add Car data to selectbox with id="make"
+    if (i === 0)
+    {
+      makeOption.text = cars[i].make;
+      makeSelectbox.appendChild(makeOption);
+    }
+    else if (cars[i].make !== cars[i - 1].make)
+    {
+      makeOption.text = cars[i].make;
+      makeSelectbox.appendChild(makeOption);
+    }
+  }
 });
+
