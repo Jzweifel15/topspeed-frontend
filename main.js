70651013402f92cff38d80bb0e2cee699db368f9
Jzweifel15 <jzweifel15@yahoo.com>
2020-12-16 19:28:57 -0500

    Front-end Blueprinting
    Creating a blueprint of how each card should look when being created by the DOM

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
+// ---------- Beginning of Class Declarations ----------
 class Car
 {
   static allCars = [];
 
-  constructor(id, make, model, year, description, imageUrl, msrp, topspeed)
+  constructor(id, make, model, year, description, imageUrl, msrp, topSpeed)
   {
     this._id = id;
     this._make = make;
@@ -11,7 +12,7 @@ class Car
     this._description = description;
     this._imageUrl = imageUrl;
     this._msrp = msrp;
-    this._topspeed = topspeed;
+    this._topSpeed = topSpeed;
     Car.allCars.push(this);
   }
 
@@ -35,6 +36,26 @@ class Car
     return this._year;
   }
 
+  get description()
+  {
+    return this._description;
+  }
+
+  get imageUrl()
+  {
+    return this._imageUrl;
+  }
+
+  get msrp()
+  {
+    return this._msrp;
+  }
+
+  get topSpeed()
+  {
+    return this._topSpeed;
+  }
+
   static get all()
   {
     return Car.allCars;
@@ -45,12 +66,22 @@ class DriversCar
 {
   static allDriversCars = [];
 
-  constructor(id, car_id, driver_id)
+  constructor(id, carId, driverId)
   {
     this._id = id;
-    this._car_id = car_id;
-    this._driver_id = driver_id;
+    this._carId = carId;
+    this._driverId = driverId;
     DriversCar.allDriversCars.push(this);
+  }
+
+  get driverId()
+  {
+    return this._driverId;
+  }
+
+  get carId()
+  {
+    return this._carId;
   }
 
   static get all()
@@ -58,6 +89,8 @@ class DriversCar
     return DriversCar.allDriversCars;
   }
 }
+// ---------- End of Class Declarations ----------
+
 
 const ROOT_URL = "http://localhost:3000/";
 const DRIVERS_URL = `${ROOT_URL}/drivers`;
@@ -65,7 +98,7 @@ const CARS_URL = `${ROOT_URL}/cars`;
 const DRIVERS_CARS_URL = `${ROOT_URL}/drivers_cars`
 
 let driversSection = document.getElementById("drivers-section");
-let driverCard = document.querySelector("div.driver.card");
+let carsGrid3 = document.getElementById("cars-grid");
 let makeSelectbox = document.querySelector("#make");
 let modelSelectbox = document.querySelector("#model");
 let yearSelectbox = document.querySelector("#year"); 
@@ -91,7 +124,6 @@ fetch(CARS_URL)
       makeSelectbox.add(makeOption);
     }
   }
-  
 });
 
 // Fetch All of Driver's Cars from backend
@@ -101,6 +133,7 @@ fetch(DRIVERS_CARS_URL)
   driversCars.forEach(driversCar => {
     new DriversCar(driversCar.id, driversCar.car_id, driversCar.driver_id);
   });
+  buildDriversCarsCards();
 });
 
 // Event Listeners
@@ -108,32 +141,10 @@ makeSelectbox.addEventListener("input", addModelsBasedOnMakeSelection);
 modelSelectbox.addEventListener("input", addYearsBasedOnModelSelection);
 document.querySelector("form").addEventListener("submit", function(e) {
   e.preventDefault();
-  let selectedCar = getTheSelectedCarsId();
-
-  let configObj = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-      "Accept": "application/json"
-    },
-    body: JSON.stringify({
-      car_id: selectedCar,
-      driver_id: 1
-    })
-  }
-
-  console.log(configObj);
-  
-  fetch(DRIVERS_CARS_URL, configObj)
-  .then(response => response.json())
-  .then(data => {
-    console.log("Success: ", data);
-  })
-  .catch(error => {
-    console.error("Error: ", error);
-  });
+  addNewCarToDatabaseForDriver();
 });
 
+// Callback functions for event-listeners
 function addModelsBasedOnMakeSelection()
 {
   if (makeSelectbox.value !== "Choose Make")
@@ -186,4 +197,100 @@ function getTheSelectedCarsId()
   }
 }
 
+function addNewCarToDatabaseForDriver()
+{
+  let selectedCar = getTheSelectedCarsId();
+
+  let configObj = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Accept": "application/json"
+    },
+    body: JSON.stringify({
+      car_id: selectedCar,
+      driver_id: 1
+    })
+  }
+
+  console.log(configObj);
+  
+  fetch(DRIVERS_CARS_URL, configObj)
+  .then(response => response.json())
+  .then(data => {
+    console.log("Success: ", data);
+  })
+  .catch(error => {
+    console.error("Error: ", error);
+  });
+}
+
+function buildDriversCarsElements(year, make, model, imageUrl, description, msrp, topSpeed)
+{
+  let carCard = document.createElement("div");
+  let cardHeading = document.createElement("h3");
+  let cardImg = document.createElement("img");
+  let cardDesc = document.createElement("p");
+  let carCardIcons = document.createElement("div");
+  let msrpIcon = document.createElement("i");
+  let topSpeedIcon = document.createElement("i");
+  let msrpLabel = document.createElement("strong");
+  let topSpeedLabel = document.createElement("strong");
+  let formForTrashBtn = document.createElement("form");
+  let formTrashBtn = document.createElement("button");
+  let trashIcon = document.createElement("i");
+
+  // Set new element attributes
+  carCard.className = "car card";
+  carCardIcons.className = "car-card icons";
+  msrpIcon.className = "fas fa-money-check-alt fa-2x";
+  topSpeedIcon.className = "fas fa-tachometer-alt fa-2x";
+  trashIcon.className = "fas fa-trash-alt fa-2x";
+  cardHeading.innerText = `${year} ${make} ${model}`;
+  cardImg.src = `${imageUrl}`;
+  cardDesc.innerText = `${description}`;
+  msrpLabel.innerText = `$${msrp}`;
+  topSpeedLabel.innerText = `${topSpeed}mph`;
+  formTrashBtn.type = "submit";
+
+  // Add Elements to proper containers
+  formTrashBtn.appendChild(trashIcon);
+
+  formForTrashBtn.appendChild(msrpIcon);
+  formForTrashBtn.appendChild(msrpLabel);
+  formForTrashBtn.appendChild(topSpeedIcon);
+  formForTrashBtn.appendChild(topSpeedLabel);
+  formForTrashBtn.appendChild(formTrashBtn);
+
+  carCardIcons.appendChild(formForTrashBtn);
+
+  carCard.appendChild(cardHeading);
+  carCard.appendChild(cardImg);
+  carCard.appendChild(cardDesc);
+  carCard.appendChild(carCardIcons);
+
+  carsGrid3.appendChild(carCard);
+}
+
+function buildDriversCarsCards()
+{
+  for (let i = 0; i < DriversCar.all.length; i++)
+  {
+    let currentDriversCar = DriversCar.all[i];
+    if (currentDriversCar.driverId === 1)
+    {
+      for (let j = 0; j < Car.all.length; j++)
+      {
+        let currentCar = Car.all[j];
+        if (currentCar.id === currentDriversCar.carId)
+        {
+          buildDriversCarsElements(currentCar.year, currentCar.make, currentCar.model, currentCar.imageUrl, currentCar.description, currentCar.msrp, currentCar.topSpeed);
+        }
+      }
+    }
+  }
+}
+
+// Debugging Area
+console.log(Car.all);
 console.log(DriversCar.all);
